df342c9e1159373c8efd8cb49d215986b65e6fa9
Twomorehearts98 <ssen98123@gmail.com>
2022-10-29 22:52:20 +0900

    .

--- app/src/main/java/umc/hufs/newsapplication/MainActivity.kt
+++ app/src/main/java/umc/hufs/newsapplication/MainActivity.kt
@@ -2,13 +2,13 @@ package umc.hufs.newsapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import umc.hufs.newsapplication.databinding.ActivityMainBinding
+import umc.hufs.newsapplication.databinding.CategoriesBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding:CategoriesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = CategoriesBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
