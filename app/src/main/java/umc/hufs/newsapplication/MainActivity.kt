b1a2af2e53d791acf489623386866aa2688e4904
Twomorehearts98 <ssen98123@gmail.com>
2022-10-29 22:19:38 +0900

    일단 시작해보기

--- app/src/main/java/umc/hufs/newsapplication/MainActivity.kt
+++ app/src/main/java/umc/hufs/newsapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package umc.hufs.newsapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import umc.hufs.newsapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
